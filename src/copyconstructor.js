7865dffe263ceefbf8de6131d4b15002da26ee9f
Ryan Pridgeon <ryan.pridgeon@britishgas.co.uk>
2018-11-22 16:30:12 +0000

    Working copy constructor builder

--- src/copyconstructor.js
+++ src/copyconstructor.js
@@ -1,8 +1,19 @@
 function CopyConstructor(input) {
 
     console.log("Hello");
-    return "123";
+    let fieldNames = input.split("\n").map(line => line.split(/\s/)[0]);
+    let builderLines = fieldNames.map(toBuilderLine);
+    return "  // Copy constructor\n"
+    + "  public X(X source) {\n"
+    + "    return X.builder()\n"
+    + "      " + builderLines.reduce((previous, current) => previous + "\n      " + current) + "\n"
+    + "      .build();\n"
+    + "  }";
     
+}
+
+function toBuilderLine(fieldName) {
+    return "." + fieldName + "(" + "source.get" + fieldName[0].toUpperCase() + fieldName.slice(1) + "())";
 }
 
 export default CopyConstructor;
